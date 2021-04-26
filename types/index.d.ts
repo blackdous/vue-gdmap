1caddaf99f88a8e194cde54b3fa668ae9606fd49
velen <896662364@qq.com>
2021-04-26 23:33:36 +0800

    feat: upate types

---
+++ types/index.d.ts
@@ -0,0 +1,14 @@
+
+export type ArrayLngLat = [number, number];
+
+export interface LngLatPos {
+  lng: number
+  lat: number
+}
+
+export interface FullLngLatPost {
+  longitude: number
+  latitude: number
+}
+
+// export interface
