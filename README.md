0dd45c3dfa762856ec600db1321d410dbefcaaa4
blackdous <zhangjingguo@shinho.net.cn>
2021-04-27 15:24:23 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# vue-bmap
-bmap vue npm
+## vue-gdmap
+
+[Vue 3.0](https://v3.vuejs.org/) + [TypeScript](https://www.tslang.cn/docs/handbook/basic-types.html) + [高德 地图](https://lbs.amap.com/api/javascript-api/summary/) 封装常用的高德地图组件。
+
+### Use
+
+
--- packages/utils/injectAmap.ts
+++ packages/utils/injectAmap.ts
@@ -2,5 +2,21 @@ const DEATUL_CONFIG = {
   v: '1.4.15',
   key: '5e3fd93ad36d3bd37412aa98a0cc3aef',
   hostAndPath: 'webapi.amap.com/maps',
-  plugin: [],
+  plugins: [],
+  callback: 'init_callback',
+  useAmapUI: false,
+  protocol: 'https',
+}
+
+interface Config {
+  v?: string
+  key?: string
+}
+
+export default class LoadAmapApi {
+  private config:Config
+  constructor(options) {
+    this.config = { ...options, ...DEATUL_CONFIG }
+    console.log('this.config: ', this.config)
+  }
 }
